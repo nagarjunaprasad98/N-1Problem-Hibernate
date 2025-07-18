b19dfab2c591eefbc70abd0ff606b2769e988c78
nagarjunaprasad98 <nagarjunaprasad99@gmail.com>
2025-07-18 14:44:48 +0530

    added chaching

--- src/main/java/NPlue1Problem/NPlue1ProblemApplication.java
+++ src/main/java/NPlue1Problem/NPlue1ProblemApplication.java
@@ -2,10 +2,12 @@ package NPlue1Problem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 // N + 1 Problem in Hibernate
 
 @SpringBootApplication
+@EnableCaching
 public class NPlue1ProblemApplication {
 
 	public static void main(String[] args) {
--- src/main/java/NPlue1Problem/service/EmployeeService.java
+++ src/main/java/NPlue1Problem/service/EmployeeService.java
@@ -3,6 +3,8 @@ package NPlue1Problem.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import NPlue1Problem.entities.Employee;
@@ -14,10 +16,12 @@ public class EmployeeService {
 	@Autowired
 	private EmployeeRepo emprepo;
 
+	@CacheEvict(value = "employees", allEntries = true)
 	public Employee save(Employee request) {
 		return emprepo.save(request);
 	}
 
+	@Cacheable("employees")
 	public List<Employee> employees() {
 		return emprepo.findEmployeeWithAddress();
 	}
